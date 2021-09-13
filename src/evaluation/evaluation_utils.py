8fa2244533c88b1f14da922d43713864fac75822
Jeroen Weener <jweener1996@gmail.com>
2021-09-13 13:40:18 +0200

    Update evalutions

--- src/evaluation/evaluation_utils.py
+++ src/evaluation/evaluation_utils.py
@@ -1,11 +1,10 @@
 # Python imports
 from typing import Tuple, List
 
-
+NUMBER_OF_RUNS = 100
 MAX_DATA_SIZE = 100000
-NUMBER_OF_RUNS = 10
+KEYWORD_LENGTH = 5
 ZN_FP_RATE = .01
-ZN_KEYWORD_LENGTH = 5
 ZN_KEY_LENGTH = 2048
 LIBERTAS_KEY_LENGTH = 256
 
@@ -15,9 +14,11 @@ def generate_data(
 ) -> List[Tuple[int, str]]:
     """Generates at most 100,000 document-keyword pairs in the form ((0, '00000'), (1, '00001'), ..., (99999, '99999')).
 
+    :param data_size: The number of document-keyword pairs to be generated
+    :type data_size: int
     :returns: List[Tuple[int, str]]
     :rtype: A specified number of document-keyword pairs
     """
     if data_size > MAX_DATA_SIZE:
         raise AssertionError('Provided data size exceeds maximum')
-    return [(n, str(n).zfill(ZN_KEYWORD_LENGTH)) for n in range(data_size)]
+    return [(n, str(n).zfill(KEYWORD_LENGTH)) for n in range(data_size)]
--- src/evaluation/wildcard_query_search.py
+++ src/evaluation/wildcard_query_search.py
@@ -1,9 +1,13 @@
 # Python imports
+import cProfile
+import math
+import pstats
 import random
 import timeit
 
 # Third-party imports
 import matplotlib.pyplot as plt
+from typing import List, Tuple
 
 # Project imports
 from evaluation.evaluation_utils import generate_data, NUMBER_OF_RUNS, ZN_FP_RATE, KEYWORD_LENGTH, \
@@ -16,28 +20,30 @@ from zhao_nishide.zn_server import ZNServer
 SEED_VALUE = 314
 
 
-class ExactKeywordSearchExperiment:
+class WildcardQuerySearchExperiment:
     def __init__(
             self,
     ) -> None:
-        index_sizes = [100, 1000, 10000, 100000]
+        matching_keywords_array = [1, 10, 100, 1000, 10000]
+        index_size = 100 #10000
 
         random.seed(SEED_VALUE)
 
-        for index_size in index_sizes:
+        for matching_keywords in matching_keywords_array:
             queries = [str(random.randint(0, index_size - 1)).zfill(KEYWORD_LENGTH) for _ in range(10)]
+            number_of_wildcards = int(math.log10(matching_keywords))
+            queries = list(map(lambda q: q[:KEYWORD_LENGTH - number_of_wildcards] + '_' * number_of_wildcards, queries))
 
-            print('Running measurements for index size', index_size)
+            print('Running measurements for', matching_keywords, 'matching',
+                  'keyword' if matching_keywords == 1 else 'keywords')
 
-            (client, server) = self.prepare_libertas(index_size)
+            data_set = generate_data(index_size)
+            (client_zn, server_zn, client_lib, server_lib) = self.prepare_schemes(data_set)
+            search_times_zn = []
             search_times_lib = []
             for query in queries:
-                search_times_lib.append(self.measure_libertas(client, server, query))
-
-            (client, server) = self.prepare_zn(index_size)
-            search_times_zn = []
-            for query in queries:
-                search_times_zn.append(self.measure_zn(client, server, query))
+                search_times_lib.append(self.measure_libertas(client_lib, server_lib, query))
+                search_times_zn.append(self.measure_zn(client_zn, server_zn, query))
 
             print('ZN:       ', list(map(lambda time: '{:.3f}'.format(time), search_times_zn)))
             print('Libertas: ', list(map(lambda time: '{:.3f}'.format(time), search_times_lib)))
@@ -49,35 +55,39 @@ class ExactKeywordSearchExperiment:
 
             print()
 
-    def prepare_zn(
+    def prepare_schemes(
             self,
-            data_size: int,
-    ) -> (ZNClient, ZNServer):
-        client = ZNClient(ZN_FP_RATE, KEYWORD_LENGTH)
-        client.setup(ZN_KEY_LENGTH)
+            data_set: List[Tuple[int, str]],
+    ) -> (ZNClient, ZNServer, LibertasClient, LibertasServer):
+        client_zn = ZNClient(ZN_FP_RATE, KEYWORD_LENGTH)
+        client_zn.setup(ZN_KEY_LENGTH)
+        server_zn = ZNServer()
+        server_zn.build_index()
 
-        server = ZNServer()
-        server.build_index()
-
-        data_set = generate_data(data_size)
+        client_lib = LibertasClient(ZNClient(ZN_FP_RATE, KEYWORD_LENGTH))
+        client_lib.setup((LIBERTAS_KEY_LENGTH, ZN_KEY_LENGTH))
+        client_lib.sigma.k = client_zn.k  # Overwrite the key of ZN so it is exactly the same
+        server_lib = LibertasServer(ZNServer())
+        server_lib.build_index()
 
         for (ind, w) in data_set:
-            add_token = client.add_token(ind, w)
-            server.add(add_token)
+            add_token = client_zn.add_token(ind, w)
+            server_zn.add(add_token)
 
-        return client, server
+            add_token = client_lib.add_token(ind, w)
+            server_lib.add(add_token)
+
+        return client_zn, server_zn, client_lib, server_lib
 
     def prepare_libertas(
             self,
-            data_size: int,
+            data_set: List[Tuple[int, str]],
     ) -> (LibertasClient, LibertasServer):
         client = LibertasClient(ZNClient(ZN_FP_RATE, KEYWORD_LENGTH))
         client.setup((LIBERTAS_KEY_LENGTH, ZN_KEY_LENGTH))
 
         server = LibertasServer(ZNServer())
         server.build_index()
-
-        data_set = generate_data(data_size)
 
         for (ind, w) in data_set:
             add_token = client.add_token(ind, w)
@@ -92,8 +102,17 @@ class ExactKeywordSearchExperiment:
             query: str,
     ) -> float:
         srch_token = client.srch_token(query)
+        # print('111111111111111111111111111111111111111111111111')
+        # profiler = cProfile.Profile()
+        # profiler.enable()
+        # for i in range(100):
+        #     self.test_zn(server, srch_token)
+        # profiler.disable()
+        # stats = pstats.Stats(profiler).sort_stats('cumtime')
+        # stats.print_stats()
         t = timeit.Timer(lambda: self.test_zn(server, srch_token))
         return t.timeit(NUMBER_OF_RUNS) / NUMBER_OF_RUNS
+        # return 1
 
     def test_zn(
             self,
@@ -109,8 +128,17 @@ class ExactKeywordSearchExperiment:
             query: str,
     ) -> float:
         srch_token = client.srch_token(query)
+        # print('2222222222222222222222222222222222222222222')
+        # profiler = cProfile.Profile()
+        # profiler.enable()
+        # for i in range(100):
+        #     self.test_lib(client, server, srch_token)
+        # profiler.disable()
+        # stats = pstats.Stats(profiler).sort_stats('cumtime')
+        # stats.print_stats()
         t = timeit.Timer(lambda: self.test_lib(client, server, srch_token))
         return t.timeit(NUMBER_OF_RUNS) / NUMBER_OF_RUNS
+        # return 1
 
     def test_lib(
             self,
@@ -123,4 +151,4 @@ class ExactKeywordSearchExperiment:
 
 
 if __name__ == '__main__':
-    ExactKeywordSearchExperiment()
+    WildcardQuerySearchExperiment()
